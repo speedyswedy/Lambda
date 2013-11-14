ebc7c5f266815c98ccb857bded1cc282fd1eed2b
Pelle Karlsson <speedyswedy@hotmail.com>
2013-11-14 13:02:40 +0100

    =support of new api

--- code/src/main/java/se/altran/java/lambda/streamdemo/StreamDemo.java
+++ code/src/main/java/se/altran/java/lambda/streamdemo/StreamDemo.java
@@ -1,30 +1,31 @@
 package se.altran.java.lambda.streamdemo;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.streams.Streams;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamDemo {
     public static void main (String[] args) {
         int max = 24;
         System.out.format("Checking numbers [1..%d]...%n", max);
         long start = System.nanoTime();
-        System.out.format("Odd numbers: %s%n", naturalNumbersUpTo(max).stream().filter(StreamDemo::heavyIsOdd).into(new ArrayList<Integer>()));
+        System.out.format("Odd numbers: %s%n", naturalNumbersUpTo(max).stream().filter(StreamDemo::heavyIsOdd).collect(Collectors.<Integer>toList()));
         long end = System.nanoTime();
         System.out.format("Time taken: %f%n", (end - start) /1.0e9);
         start = System.nanoTime();
-        System.out.format("Odd numbers: %s%n", naturalNumbersUpTo(max).parallel().filter(StreamDemo::heavyIsOdd).into(new ArrayList<Integer>()));
+        System.out.format("Odd numbers: %s%n", naturalNumbersUpTo(max).parallelStream().filter(StreamDemo::heavyIsOdd).collect(Collectors.<Integer>toList()));
         end = System.nanoTime();
         System.out.format("Time taken: %f%n", (end - start) /1.0e9);
     }
 
     public static List<Integer> naturalNumbersUpTo(int max) {
-        return Streams.iterate(1, num -> num + 1).limit(max).into(new ArrayList<Integer>(max));
+        return Stream.iterate(1, num -> num + 1).limit(max).collect(Collectors.<Integer>toList());
+
     }
 
     public static boolean heavyIsOdd(Integer number) {
         try {
-            Thread.sleep(100);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
         }
         return number % 2 == 1;
